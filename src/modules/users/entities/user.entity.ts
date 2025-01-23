6e707369e12d12c6deed0b7a758ae4c96616f577
antony-souza <antonygustavo09@hotmail.com>
2025-01-23 15:44:17 -0300

    update teste readme

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Prop, Schema } from '@nestjs/mongoose';
+import { randomUUID } from 'crypto';
+
+@Schema({ timestamps: true, versionKey: false })
+export class User {
+  @Prop({ type: String, default: randomUUID, required: false })
+  _id: string;
+
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({ type: String, required: true, ref: 'Group' })
+  group: string;
+}
